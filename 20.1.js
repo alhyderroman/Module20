06ec856d0b7e0ad858edbba9973993372e41c6b1
alhyderroman <alhyderroman807@yahoo.com>
2024-02-03 11:53:19 +0600

    message

---
+++ 20.1.js
@@ -0,0 +1,6 @@
+function product(p1,p2){
+    return p1*p2;
+
+}
+
+console.log(product(4,3));
---
+++ 20.2.js
@@ -0,0 +1,12 @@
+function square(a){
+console.log("Given number is:",a);
+const result=a*a;
+console.log("Square of the given number:",result);
+}
+
+square(8);
+
+square(9.001);
+square(785);
+
+
